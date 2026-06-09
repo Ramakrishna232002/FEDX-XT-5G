ed7fd74c7322877e8777ff80765771af5f1aab5f
Ramakrishna232002 <ramakrishna232002@gmail.com>
2026-06-09 10:25:35 +0530

    Step

--- step_command.py
+++ step_command.py
@@ -1,5 +1,3 @@
-streamlit run frontend/app.py
-python backend/app.py
 
 1. NEXUS Sentinel 
 
@@ -11,3 +9,40 @@ Sentinel = Security guardian
 Tagline:
 
 Intelligent Threat Detection for Next-Generation Networks
+
+🚀 Step-by-Step Setup Guide--
+
+Step 1: Clone the Repository
+git clone https://github.com/Ramakrishna232002/FedX-ZT-5G.git
+cd FedX-ZT-5G
+
+Step 2: Create Virtual Environment
+python -m venv venv
+venv\Scripts\activate
+
+Step 3: Install Dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+Step 4: Download Dataset
+https://www.unsw.adfa.edu.au/unsw-canberra-cyber/cybersecurity/ADFA-NB15-Datasets/
+
+Step 5: Train the Federated Model (First Time Only)
+python src/federated_final.py
+
+This will:
+
+Preprocess the data
+
+Split into 3 edge nodes
+
+Train federated models
+
+Save to models/saved/federated_models.pkl
+
+Step 6: Run the Application
+Terminal 1 - Backend API:
+    python backend/app.py
+Terminal 2 - Frontend UI:
+    streamlit run frontend/app.py
+    
